7de694f70c8f995b11cee8d06985fa310905535f
Tri Dao <tridpq@gmail.com>
2023-08-11 21:57:47 -0700

    Use CUDA network install

--- flash_attn/__init__.py
+++ flash_attn/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2.0.8.post3"
+__version__ = "2.0.8.post4"
 
 from flash_attn.flash_attn_interface import flash_attn_func
 from flash_attn.flash_attn_interface import flash_attn_kvpacked_func
